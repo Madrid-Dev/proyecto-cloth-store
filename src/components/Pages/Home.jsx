b197eb361a0f5c3d8b6a90ed09e3b0429f23b31e
Agustín Madrid <agusmadrid01@gmail.com>
2023-05-17 13:06:09 -0300

    Clothes images added

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './../../styles/home.css';
+import thrasher from './../../images/clothes/thrasher.jpg';
+import top from './../../images/clothes/top.jpg';
+import orangeTShirt from './../../images/clothes/orange-t-shirt.jpg';
+import lanaSweater from './../../images/clothes/lana-sweater.jpg';
+import cottomSweater from './../../images/clothes/cottom-cyan-sweater.jpg';
+
+const Home = () => {
+    return (
+        <div className = 'container'>
+            <div className = 'grid-container'>
+
+            </div>
+
+        </div>
+    );
+}
+ 
+export default Home;
