bef4cd3e553d8fa79fa7706b2bfbf00fe5540029
amadrid01 <agusmadrid01@gmail.com>
2023-06-25 01:44:03 -0300

    logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import logo from './logo.svg';
+import logo from './images/Elevated-logos.jpeg';
 import './App.css';
 import Header from './components/Header';
-
-
+import React from "react";
+import {Helmet} from "react-helmet";
 import Home from './components/Pages/Home';
 
 function App() {
   return (
+    <>
+    <Helmet>
+      <title>Elevated | Store</title>
+    </Helmet>
     <div className="App">
 
       <div className='body'>
         <Home></Home>
       </div>
     </div>
+    </>
       
   );
 }
